288f51c1467ed9ed120b8a17c1a71db78ac2bfcd
Isaque Chaves <isaque.ramosch@gmail.com>
2024-11-15 18:22:02 -0300

    feat: add getAllImmobiles controler

--- src/main/java/seu/lar/api/controller/immobileController.java
+++ src/main/java/seu/lar/api/controller/immobileController.java
@@ -1,4 +1,25 @@
 package seu.lar.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import seu.lar.api.immobile.Immobile;
+import seu.lar.api.immobile.ImmobileRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/immobiles")
 public class immobileController {
+
+    private final ImmobileRepository immobileRepository;
+
+    public immobileController(ImmobileRepository immobileRepository) {
+        this.immobileRepository = immobileRepository;
+    }
+
+    @GetMapping
+    public List<Immobile> getAllImmobiles() {
+        return immobileRepository.findAll();
+    }
 }
